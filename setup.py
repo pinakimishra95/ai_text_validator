adb7db78a509b952b149b6917c519f034be87f6a
Pinaki <pinaki@Pinakis-MacBook-Air.local>
2023-08-19 23:55:14 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ai_text_validation',
+    version='0.1',
+    description='A Python package for validating AI generated text.',
+    author='Pinaki Mishra',
+    author_email='pinakimishra95@gmail.com',
+    # url='https://github.com/bard/ai_text_validation',
+    packages=['ai_text_validation'],
+    install_requires=['transformers'],
+)
